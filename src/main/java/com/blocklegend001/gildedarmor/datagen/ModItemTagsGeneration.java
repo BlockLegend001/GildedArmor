062936f093daa2488eb2bd25056cdecdced470ba
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 14:09:42 +0100

    Aggiunta 1.20.6-Fabric

---
+++ src/main/java/com/blocklegend001/gildedarmor/datagen/ModItemTagsGeneration.java
@@ -0,0 +1,63 @@
+package com.blocklegend001.gildedarmor.datagen;
+
+import com.blocklegend001.gildedarmor.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.blocklegend001.gildedarmor.GildedArmor.*;
+
+public class ModItemTagsGeneration extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagsGeneration(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(dataOutput, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+
+        getOrCreateTagBuilder(GILDED_NETHERITE)
+                .add(ModItems.GILDED_NETHERITE_HELMET,
+                        ModItems.GILDED_NETHERITE_CHESTPLATE,
+                        ModItems.GILDED_NETHERITE_LEGGINGS,
+                        ModItems.GILDED_NETHERITE_BOOTS);
+
+        getOrCreateTagBuilder(ItemTags.ARMOR_ENCHANTABLE)
+                .add(ModItems.GILDED_NETHERITE_HELMET,
+                        ModItems.GILDED_NETHERITE_CHESTPLATE,
+                        ModItems.GILDED_NETHERITE_LEGGINGS,
+                        ModItems.GILDED_NETHERITE_BOOTS);
+
+        getOrCreateTagBuilder(ItemTags.EQUIPPABLE_ENCHANTABLE)
+                .add(ModItems.GILDED_NETHERITE_HELMET,
+                        ModItems.GILDED_NETHERITE_CHESTPLATE,
+                        ModItems.GILDED_NETHERITE_LEGGINGS,
+                        ModItems.GILDED_NETHERITE_BOOTS);
+
+        getOrCreateTagBuilder(ItemTags.DURABILITY_ENCHANTABLE)
+                .add(ModItems.GILDED_NETHERITE_HELMET,
+                        ModItems.GILDED_NETHERITE_CHESTPLATE,
+                        ModItems.GILDED_NETHERITE_LEGGINGS,
+                        ModItems.GILDED_NETHERITE_BOOTS);
+
+        getOrCreateTagBuilder(ItemTags.HEAD_ARMOR_ENCHANTABLE)
+                .add(ModItems.GILDED_NETHERITE_HELMET);
+
+        getOrCreateTagBuilder(ItemTags.FOOT_ARMOR_ENCHANTABLE)
+                .add(ModItems.GILDED_NETHERITE_BOOTS);
+
+        getOrCreateTagBuilder(ItemTags.CHEST_ARMOR_ENCHANTABLE)
+                .add(ModItems.GILDED_NETHERITE_CHESTPLATE);
+
+        getOrCreateTagBuilder(ItemTags.LEG_ARMOR_ENCHANTABLE)
+                .add(ModItems.GILDED_NETHERITE_LEGGINGS);
+
+        getOrCreateTagBuilder(ItemTags.TRIMMABLE_ARMOR)
+                .add(ModItems.GILDED_NETHERITE_HELMET,
+                        ModItems.GILDED_NETHERITE_CHESTPLATE,
+                        ModItems.GILDED_NETHERITE_LEGGINGS,
+                        ModItems.GILDED_NETHERITE_BOOTS);
+    }
+}
---
+++ src/main/java/com/blocklegend001/gildedarmor/item/GildedArmorItem.java
@@ -0,0 +1,29 @@
+package com.blocklegend001.gildedarmor.item;
+
+import net.minecraft.client.item.TooltipType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import java.util.List;
+
+public class GildedArmorItem extends ArmorItem {
+
+    public GildedArmorItem(RegistryEntry<ArmorMaterial> material, Type type, Settings settings) {
+        super(material, type, settings);
+    }
+
+    @Override
+    public boolean isEnchantable(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("tooltip.gildedarmor.description").formatted(Formatting.GOLD));
+    }
+
+}
