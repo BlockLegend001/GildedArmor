5abfbba139335b1b8611ca58bd066685ccee0f67
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-24 17:09:51 +0100

    Added config

--- src/main/java/com/blocklegend001/gildedarmor/items/ModArmorMaterials.java
+++ src/main/java/com/blocklegend001/gildedarmor/items/ModArmorMaterials.java
@@ -1,6 +1,7 @@
 package com.blocklegend001.gildedarmor.items;
 
 import com.blocklegend001.gildedarmor.GildedArmor;
+import com.blocklegend001.gildedarmor.config.ModConfig;
 import com.blocklegend001.gildedarmor.util.ModEquipmentAssets;
 import net.minecraft.Util;
 import net.minecraft.resources.ResourceLocation;
@@ -12,12 +13,12 @@ import net.minecraft.world.item.equipment.ArmorType;
 import java.util.EnumMap;
 
 public class ModArmorMaterials {
-    public static final ArmorMaterial GILDED_NETHERITE= new net.minecraft.world.item.equipment.ArmorMaterial(45, Util.make(new EnumMap<>(ArmorType.class),
+    public static final ArmorMaterial GILDED_NETHERITE= new ArmorMaterial(ModConfig.durabilityValueGildedArmor, Util.make(new EnumMap<>(ArmorType.class),
             attribute -> {
-                attribute.put(ArmorType.BOOTS, 6);
-                attribute.put(ArmorType.LEGGINGS, 7);
-                attribute.put(ArmorType.CHESTPLATE, 10);
-                attribute.put(ArmorType.HELMET, 6);
-            }), 20, SoundEvents.ARMOR_EQUIP_NETHERITE, 4.0F, 0.1F, ItemTags.REPAIRS_NETHERITE_ARMOR,
+                attribute.put(ArmorType.BOOTS, ModConfig.protectionValueGildedBoots);
+                attribute.put(ArmorType.LEGGINGS, ModConfig.protectionValueGildedLeggings);
+                attribute.put(ArmorType.CHESTPLATE, ModConfig.protectionValueGildedChestplate);
+                attribute.put(ArmorType.HELMET, ModConfig.protectionValueGildedHelmet);
+            }), ModConfig.enchantmentValueGildedArmor, SoundEvents.ARMOR_EQUIP_NETHERITE, ModConfig.toughnessValueGildedArmor, ModConfig.knockbackResistanceValueGildedArmor, ItemTags.REPAIRS_NETHERITE_ARMOR,
             ModEquipmentAssets.GILDED_NETHERITE);
 }
