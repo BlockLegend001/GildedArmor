ed942377105259e41472cc98f9fdbd36183b0337
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-22 20:57:00 +0200

    Added 1.21.8-Fabric

---
+++ src/main/java/com/blocklegend001/gildedarmor/client/GildedArmorClient.java
@@ -0,0 +1,10 @@
+package com.blocklegend001.gildedarmor.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GildedArmorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
