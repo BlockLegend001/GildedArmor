12b9f17cf47bd814bf08e93a4cfbf69f768bad40
BlockLegend001 <rosarioarcamone@gmail.com>
2025-10-10 20:56:18 +0200

    Added 1.21.10-Forge

---
+++ src/main/java/com/blocklegend001/gildedarmor/item/ModArmorMaterials.java
@@ -0,0 +1,22 @@
+package com.blocklegend001.gildedarmor.item;
+
+import java.util.EnumMap;
+
+import com.blocklegend001.gildedarmor.config.ModConfig;
+import com.blocklegend001.gildedarmor.util.ModEquipmentAssets;
+import net.minecraft.Util;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.equipment.ArmorMaterial;
+import net.minecraft.world.item.equipment.ArmorType;
+
+public class ModArmorMaterials {
+    public static final ArmorMaterial GILDED_NETHERITE= new ArmorMaterial(ModConfig.durabilityValueGildedArmor, Util.make(new EnumMap<>(ArmorType.class),
+            attribute -> {
+                attribute.put(ArmorType.BOOTS, ModConfig.protectionValueGildedBoots);
+                attribute.put(ArmorType.LEGGINGS, ModConfig.protectionValueGildedLeggings);
+                attribute.put(ArmorType.CHESTPLATE, ModConfig.protectionValueGildedChestplate);
+                attribute.put(ArmorType.HELMET, ModConfig.protectionValueGildedHelmet);
+            }), ModConfig.enchantmentValueGildedArmor, SoundEvents.ARMOR_EQUIP_NETHERITE, ModConfig.toughnessValueGildedArmor, ModConfig.knockbackResistanceValueGildedArmor, ItemTags.REPAIRS_NETHERITE_ARMOR,
+            ModEquipmentAssets.GILDED_NETHERITE);
+}
